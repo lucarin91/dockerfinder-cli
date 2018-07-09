7c199a5e63fba6e01de81867916a3af528649c31
lucarin91 <lucarin91@gmail.com>
2018-07-09 18:08:48 +0200

    add new dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'tabulate']
+requirements = ['Click>=6.0', 'tabulate', 'requests']
 
 setup_requirements = [ ]
 
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lucarin91/dockerfinder-cli',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
